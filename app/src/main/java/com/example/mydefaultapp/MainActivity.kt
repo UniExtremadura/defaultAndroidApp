f399181a362ccd15b39eff3a82641d4b88d2747c
Javier Berrocal <jberolm@unex.es>
2023-11-01 22:19:46 +0100

    Update MainActivity.kt
    Minor changes for testing

--- app/src/main/java/com/example/mydefaultapp/MainActivity.kt
+++ app/src/main/java/com/example/mydefaultapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mydefaultapp.databinding.ActivityMainBinding
 
+//Minor change for teting
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
